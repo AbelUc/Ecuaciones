b7d98336bb38820fe31108a155658ba22ed8a959
Abel <abelmihjail@gmail.com>
2022-03-24 21:37:43 -0600

    Initial commit

---
+++ app/src/main/java/com/example/ecuaciones/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.ecuaciones
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        var usu="Abel"
+        var contra="Mihjail18"
+
+        var txtusu=findViewById<EditText>(R.id.txtusu)
+        var txtxpass=findViewById<EditText>(R.id.txtxcontra)
+
+        var blogin=findViewById<Button>(R.id.blogin)
+
+        blogin.setOnClickListener{
+
+            if(txtusu.text.toString()==usu&&txtxpass.text.toString()==contra){
+                var intent= Intent(this, Login::class.java)
+                intent.putExtra("mensaje", "Sesión iniciada con éxito")
+                startActivity(intent)
+            }
+            else{
+                Toast.makeText(this,"Los datos son incorrectos",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
